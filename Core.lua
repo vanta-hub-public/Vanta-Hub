30a3a6723936f8a8f5eeda54bacb703700c79cfd
vanta-hub-public <2in6fr@gmail.com>
2026-09-20 01:10:20 +0300

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -6,7 +6,7 @@ local function fetch(url) return game:HttpGet(url) end
 local Library = loadstring(fetch(LINORIA .. "Library.lua"))()
 local SaveManager = loadstring(fetch(LINORIA .. "addons/SaveManager.lua"))()
 
-local VERSION = "v0.1"
+local VERSION = "v0.3"
 local CONFIG_NAME = "autosave"
 
 Library.FontColor = Color3.fromHex("ffffff")
